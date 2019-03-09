f20290ed1240d93b6b23eefcda5149371293641a
unknown <you@example.com>
2019-03-09 13:15:22 +0900

    [Fix]アプリケーション作成基礎

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -12,8 +12,8 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
     if book.save
-     # flash[:notice] = "successfully"
-  	redirect_to book_path(book.id), notice: "successfully"
+    flash[:notice] = "Book was successfully created."
+  	redirect_to book_path(book.id)
     else
       render :new
   end
@@ -26,7 +26,8 @@ end
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-    redirect_to book_path(book.id), notice: "successfully"
+      flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
   else
     render :edit
   end
@@ -34,6 +35,7 @@ end
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
